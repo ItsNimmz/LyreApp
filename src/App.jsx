713a7fbfb5dcebdce64e62b107fdc950e782528b
nimisha <nimishanimmz@gmail.com>
2024-06-28 19:04:04 -0400

    added sidebar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import SidebarComponent from './components/SidebarComponent'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
         <div className='h-[90%] flex'>
-
+          <SidebarComponent />
         </div>
     </div>
   )
