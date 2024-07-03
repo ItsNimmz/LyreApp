02d6549531882ba822a20ec26efee97a2d72c9a9
Anju Ann Sunny <annsunny126@gmail.com>
2024-07-03 08:55:20 -0400

    new heading featured charts

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import NavBar from './NavBar'
-import { albumsData } from '../assets/assets'
+import React from 'react';
+import NavBar from './NavBar';
+import { albumsData } from '../assets/assets';
+import Albumlist from './Albumlist';
 
 const DisplayHome = () => {
   return (
     <>
-      <NavBar/>
-      
+      <NavBar />
+      <div className='mb-4'>
+        <h1 className='my-5  font-bold text-2xl'>Featured Charts</h1>
+        <div className='flex overflow-auto'>
+        {albumsData.map((item, index) => (
+          <Albumlist key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />
+        ))}
+        </div>
+        
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
