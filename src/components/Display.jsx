f2966408f9f7612fac017dcae23229c8f0c1b654
nikithamarythomas <nikithamarythomas@gmail.com>
2024-07-04 00:49:17 -0400

    Redirect URL modified

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -28,7 +28,7 @@ const Display = () => {
   return (
     <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       <Routes>
-        <Route path='/' element={<DisplayHome />}/>
+        <Route path='/callback?' element={<DisplayHome />}/>
         {/* <Route path='/album/:id' element={<DisplayAlbum />}/> */}
       </Routes>
     </div>
