48fdac0ed3cae7ea59748ca508d05cdc6c46f05c
nimisha <nimishanimmz@gmail.com>
2024-08-05 15:29:15 -0400

    testing-5

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -15,7 +15,7 @@ export const getAccessToken =  () => {
 
     const postData = {
       grant_type: 'authorization_code',
-      code: token,
+      code: localStorage.getItem('code'),
       redirect_uri: REDIRECT_URI,
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET,
