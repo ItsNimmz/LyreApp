c0d203fbcaa9566cdd0a7e75fb2729f722a75e65
nikithamarythomas <nikithamarythomas@gmail.com>
2024-08-07 14:12:39 -0400

    button add

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -166,9 +166,9 @@ const NavBar = () => {
             </div>
           </Modal>
         </div>
-          <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>
-            Explore Premium
-          </p>
+        <button onClick={() => window.open('https://cap2-emotion-detection1.onrender.com/recommender', '_blank')} className='bg-black px-4 py-1 rounded-2xl text-white cursor-pointer'>
+                Recommender
+          </button>
           <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>
             Install App
           </p>
@@ -188,9 +188,7 @@ const NavBar = () => {
       <div className='flex items-center gap-2 mt-4'>
         <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>All</p>
         <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
-        <button onClick={() => window.open('https://cap2-emotion-detection1.onrender.com/recommender', '_blank')} className='bg-black px-4 py-1 rounded-2xl text-white cursor-pointer'>
-                Recommender
-          </button>
+
       </div>
       <Modal
         isOpen={showAccInfo}
